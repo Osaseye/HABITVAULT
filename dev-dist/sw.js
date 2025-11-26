bc5344e862c8b52e378d1bd03cf41b0cde5d0a02
Osaseye <sadebowale092@gmail.com>
2025-11-26 03:29:34 +0100

    Finished v2

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ce4f0d5f'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.psmie7odjf8"
+    "revision": "0.o0kjm9m6t98"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
